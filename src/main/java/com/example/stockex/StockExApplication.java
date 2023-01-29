40e6060b2ab7c427cb5b3bfb1c1ef1b52c85cf17
YeonJae Jeong <hkzxcv12@naver.com>
2023-01-29 23:02:35 +0900

    init project

---
+++ src/main/java/com/example/stockex/StockExApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockExApplication.class, args);
+	}
+
+}
